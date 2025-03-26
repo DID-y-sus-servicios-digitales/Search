12f6420920c3c5a874c34c3062a99c3fe54dbb45
Daniel <yutencurso@gmail.com>
2025-03-26 08:16:25 +0100

    Update refrán.js

--- refrán.js
+++ refrán.js
@@ -17,3 +17,18 @@ function refranDeLaSemana() {
 
 // Mostrar el refrán en el elemento HTML
 document.getElementById("refran").innerText = refranDeLaSemana();
+const textoInput = document.getElementById('textoInput');
+        const generarBtn = document.getElementById('generarBtn');
+        const qrcodeDiv = document.getElementById('qrcode');
+        const descargaLink = document.getElementById('descargaLink');
+
+        generarBtn.addEventListener('click', () => {
+            const texto = textoInput.value;
+            if (texto) {
+                qrcodeDiv.innerHTML = ''; // Limpiar el QR anterior
+                new QRCode(qrcodeDiv, texto);
+                descargaLink.style.display = 'block'; // Mostrar el enlace de descarga
+                descargaLink.href = qrcodeDiv.querySelector('img').src; // Obtener la URL del QR
+                descargaLink.download = 'didsearch qrcode.png'; // Nombre del archivo de descarga
+            }
+        });
